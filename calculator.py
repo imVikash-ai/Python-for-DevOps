17adcb37425d5418a40ca8455e576f690ac048b2
Vikash Kumar <kumarvikash97716880@gmail.com>
2026-08-17 22:03:22 +0530

    calculator.py add

---
+++ calculator.py
@@ -0,0 +1,16 @@
+num1 = int(input("Enter first number: "))
+num2 = int(input("Enter second number: "))
+
+# print(type(num1))
+
+sum_of_number = num1 + num2
+print("The sum of the two numbers is: ", sum_of_number)
+
+diff_of_num = num1 - num2
+print("The difference of the two numbers is: ", diff_of_num)
+
+product_of_num = num1 * num2
+print("The product of the two numbers is: ", product_of_num)
+
+division_of_num = num1 / num2
+print("The division of the two numbers is: ", division_of_num)
