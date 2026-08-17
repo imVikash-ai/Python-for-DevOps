7c788c13fc448dfb5b5f22c2a6ee140c19d07f6a
Vikash Kumar <kumarvikash97716880@gmail.com>
2026-08-17 22:38:40 +0530

    loops.py file added

---
+++ loops.py
@@ -0,0 +1,22 @@
+# list_of_cloud = ["AWS", "Azure", "GCP","utho","IBM"]
+
+# print(list_of_cloud)
+
+# list_of_cloud.append("salesforce")
+
+# print(list_of_cloud)
+
+# list_of_cloud.insert(2,"Heroku")
+
+# print(list_of_cloud)
+
+# list_of_cloud.insert(0,"HELLO Cloud")
+
+# print(list_of_cloud)
+# print(len(list_of_cloud))
+
+# for cloud in list_of_cloud:
+#     print(cloud)
+
+for i in range(0,12):
+    print("Hello dosto" ,i)
